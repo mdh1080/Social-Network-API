6e6ab7d83f1d4678589fe99a8bde2f1fc135664d
@mdh1080 <110183347+mdh1080@users.noreply.github.com>
2023-01-30 12:53:03 -0500

    Move server.js to Develop folder

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const db = require('./Develop/config/connection');
-const routes = require('./Develop/routes');
+const db = require('./config/connection');
+const routes = require('./routes');
 
 const cwd = process.cwd();
 
