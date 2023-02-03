f02bc7e7da0937ad6b7473733ab4dba37a9a23a0
@mdh1080 <110183347+mdh1080@users.noreply.github.com>
2023-02-02 21:26:55 -0500

    Modify package json and user route

--- Develop/routes/api/userRoutes.js
+++ Develop/routes/api/userRoutes.js
@@ -1,19 +1,21 @@
 const router = require('express').Router();
 
 const {
-  getUsers,
+  getUser,
   getSingleUser,
   updateUser,
   createUser,
   deleteUser,
-  createFriend,
+  addFriend,
   deleteFriend,
 } = require('../../controllers/userController');
 
-router.route('/').get(getUsers).post(createUser);
+router.route('/').get(getUser).post(createUser);
 
 router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
 
-router.route('/:userId/friends/:friendId').post(createFriend).delete(deleteFriend);
+router.route('/:userId/friends').post(addFriend);
+
+router.route('/:userId/friends/:friendId').delete(deleteFriend);
 
 module.exports = router;
